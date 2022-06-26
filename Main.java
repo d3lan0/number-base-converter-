dac8c81921db5c1a0fe98c9c324d9f351f5f88c5
Theo Higgins <theo.higgins@Theos-MacBook-Pro.local>
2022-06-25 22:29:58 -0400

    Stage 2 completed. Converts to and from decimal and prompts until user enters /exit.

--- Main.java
+++ Main.java
@@ -7,25 +7,62 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
         Scanner scanner = new Scanner(System.in);
+        StringBuilder command = new StringBuilder();
+        do {
+            System.out.print("Do you want to convert /from decimal or /to decimal? (To quit type /exit)");
+            command.replace(0, command.length(), scanner.next());
 
-        System.out.print("Enter number in decimal system: ");
-        int decimal = scanner.nextInt();
+            if("/exit".contentEquals(command)) {
+               System.out.println("/exit");
+               return;
+            }
 
-        System.out.print("Enter target base: ");
-        int base = scanner.nextInt();
+            if("/from".contentEquals(command)) {
+                System.out.print("Enter number in decimal system: ");
+                int decimal = scanner.nextInt();
+
+                System.out.print("Enter target base: ");
+                int base = scanner.nextInt();
+
+                System.out.println(convertFromDecimal(decimal, base));
+            }
+
+            if("/to".contentEquals(command)) {
+                System.out.println("Enter source number: ");
+                String source = scanner.next();
+
+                System.out.println("Enter source base: ");
+                int base = scanner.nextInt();
+
+                System.out.println(convertToDecimal(base, source));
+            }
+
+        } while (!"/exit".contentEquals(command));
 
 
+
+    }
+    public static StringBuilder convertFromDecimal(int decimal, int base) {
+        StringBuilder string = new StringBuilder("Conversion result: ");
         if (base == 2) {
-            System.out.println("Conversion result: " + Integer.toBinaryString(decimal));
+            string.append(Integer.toBinaryString(decimal));
         }
 
         if(base == 8) {
-            System.out.println("Conversion result: " + Integer.toOctalString(decimal));
+            string.append(Integer.toOctalString(decimal));
         }
 
         if(base == 16) {
-            System.out.println("Conversion result: " + Integer.toHexString(decimal));
+            string.append(Integer.toHexString(decimal));
         }
 
+        return string;
+    }
+
+    public static StringBuilder convertToDecimal(int base, String value){
+        StringBuilder string = new StringBuilder("Conversion to decimal result: ");
+            string.append(Integer.parseInt(value, base));
+        return string;
     }
 }
+
